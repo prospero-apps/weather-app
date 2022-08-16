c2208228119d37115f6b8285c825dad724dc2438
Kamil <prospero.apps@gmail.com>
2022-08-17 01:13:46 +0200

    Implement 5-day forecast

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import './style.css';
-import CloudyIcon from './icons/clouds.png';
-// import FoggyIcon from './icons/weather-fog.png';
-// import HailyIcon from './icons/weather-hail.png';
-// import StormyIcon from './icons/weather-lighning.png';
-// import OvercastIcon from './icons/weather-partly-cloudy.png';
-// import RainyIcon from './icons/weather-pouring.png';
-// import SnowyIcon from './icons/weather-snowy-heavy.png';
-// import SunnyIcon from './icons/weather-sunny.png';
-// import WindyIcon from './icons/weather-dust.png';
 
 import {
   city,
@@ -31,50 +22,6 @@ function addItem(where, cls, tag = 'div') {
   return where.appendChild(element);
 }
 
-// location panel
-// city.textContent = 'New York';
-// todayDate.textContent = 'Monday, August 15, 2022';
-// todayTime.textContent = '7:47 pm';
-
-// temperature panel
-// description.textContent = 'Light Rain';
-// const weatherIcon = new Image();
-// weatherIcon.src = CloudyIcon;
-// weatherImage.appendChild(weatherIcon);
-// temperature.textContent = '24°C';
-
-// stats panel
-// feelsLikeValue.textContent = '22°C';
-// windSpeedValue.textContent = '28.6 km/h';
-// rainChanceValue.textContent = '0.6%';
-// humidityValue.textContent = '84%';
-
-// week panel
-for (let i = 0; i < 7; i++) {
-  const dayContainer = addItem(weekPanel, 'week-container');
-  const day = addItem(dayContainer, 'day');
-  day.textContent = 'Thursday';
-  const dayTemperature = addItem(dayContainer, 'day-temperature');
-  dayTemperature.textContent = '19°C';
-  const dayIcon = new Image();
-  dayIcon.className = 'day-icon';
-  dayIcon.src = CloudyIcon;
-  dayContainer.appendChild(dayIcon);
-}
-
-async function getGif(location) {
-  fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=W7cKIBKFMbbAP5dAAkviacBwQNLWQZR6&s=${location}`,
-    { mode: 'cors' },
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      gif.src = response.data.images.original.url;
-    });
-}
-
-// getGif('New York');
-
 function formatDate(timezone) {
   const localTime = new Date();
   const timeOffset = localTime.getTimezoneOffset() * 60 + timezone;
@@ -95,7 +42,20 @@ function celsiusToFahrenheit(celsius) {
   return Math.round(celsius * 1.8 + 32);
 }
 
+async function getGif(location) {
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=W7cKIBKFMbbAP5dAAkviacBwQNLWQZR6&s=${location}`,
+    { mode: 'cors' },
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      gif.src = response.data.images.original.url;
+    });
+}
+
+// LOAD WEATHER DATA
 async function getData(location) {
+  // CURRENT WEATHER
   const response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=48f6a27fca74b448539664c245fa0a54`,
     { mode: 'cors' },
@@ -106,17 +66,6 @@ async function getData(location) {
     { mode: 'cors' },
   );
 
-  const forecast = await fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?q=${location}&APPID=48f6a27fca74b448539664c245fa0a54`,
-    { mode: 'cors' },
-  );
-
-  const forecastData = await forecast.json();
-
-  console.log(data);
-  console.log(forecastData);
-
-  // feed data
   // location panel
   city.textContent = data.name;
   const localTime = formatDate(data.timezone);
@@ -136,13 +85,46 @@ async function getData(location) {
   windSpeedValue.textContent = `${data.wind.speed} km/h`;
   pressureValue.textContent = `${data.main.pressure} hPa`;
   humidityValue.textContent = `${data.main.humidity}%`;
+
+  // 5-DAY FORECAST
+  const forecast = await fetch(
+    `http://api.openweathermap.org/data/2.5/forecast?q=${location}&APPID=48f6a27fca74b448539664c245fa0a54`,
+    { mode: 'cors' },
+  );
+
+  const forecastData = await forecast.json();
+  const days = [
+    forecastData.list[4],
+    forecastData.list[12],
+    forecastData.list[20],
+    forecastData.list[28],
+    forecastData.list[36],
+  ];
+
+  // console.log(data);
+  console.log(forecastData);
+
+  // week panel
+  for (let i = 0; i < 5; i++) {
+    const dayContainer = addItem(weekPanel, 'week-container');
+    const day = addItem(dayContainer, 'day');
+    day.textContent = days[i].dt_txt;
+    const dayTemperature = addItem(dayContainer, 'day-temperature');
+    dayTemperature.textContent = `${Math.round(days[i].main.temp - 273.15)}°C`;
+    const dayDescription = addItem(dayContainer, 'day-description');
+    dayDescription.textContent = days[i].weather[0].description;
+
+    const icon = await fetch(
+      `http://openweathermap.org/img/wn/${days[i].weather[0].icon}@2x.png`,
+      { mode: 'cors' },
+    );
+
+    const dayIcon = new Image();
+    dayIcon.className = 'day-icon';
+    dayIcon.src = icon.url;
+    dayContainer.appendChild(dayIcon);
+  }
 }
 
+getGif('New York');
 getData('New York');
-
-/// API KEY maps
-// http://api.openweathermap.org/data/2.5/weather?q=London&APPID=48f6a27fca74b448539664c245fa0a54
-// 48f6a27fca74b448539664c245fa0a54
-
-/// API KEY giphy
-// `https://api.giphy.com/v1/gifs/translate?api_key=W7cKIBKFMbbAP5dAAkviacBwQNLWQZR6&s=london`
