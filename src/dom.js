773ba27ac1140fe2396964e5b942ad231b18115b
Kamil <prospero.apps@gmail.com>
2022-08-16 09:55:57 +0200

    Create basic structure with all elements in place

--- src/dom.js
+++ src/dom.js
@@ -1,26 +1,86 @@
+import SearchIcon from './icons/magnify.png';
+import FeelsLikeIcon from './icons/thermometer-lines.png';
+import WindSpeedIcon from './icons/weather-windy.png';
+import RainChanceIcon from './icons/weather-rainy.png';
+import HumidityIcon from './icons/water-percent.png';
+
 // helper functions
-function createPanel(id) {
-  const panel = document.createElement('div');
-  panel.id = id;
-  return panel;
+function add(where, id, tag = 'div') {
+  const element = document.createElement(tag);
+  element.id = id;
+  return where.appendChild(element);
 }
 
 const content = document.getElementById('content');
-const container = content.appendChild(createPanel('container'));
-const temperaturePanel = container.appendChild(createPanel('temperature-panel'));
-const locationPanel = container.appendChild(createPanel('location-panel'));
-const statsPanel = container.appendChild(createPanel('stats-panel'));
-const navPanel = container.appendChild(createPanel('nav-panel'));
-const weekPanel = container.appendChild(createPanel('week-panel'));
-const hourPanel = container.appendChild(createPanel('hour-panel'));
 
-export {
-  content,
-  container,
-  temperaturePanel,
-  locationPanel,
-  statsPanel,
-  navPanel,
-  weekPanel,
-  hourPanel,
-};
+/* MAIN CONTAINER */
+const container = add(content, 'container');
+const locationPanel = add(container, 'location-panel');
+const temperaturePanel = add(container, 'temperature-panel');
+const statsPanel = add(container, 'stats-panel');
+export const dayPanel = add(container, 'day-panel');
+export const weekPanel = add(container, 'week-panel');
+
+/* LOCATION PANEL */
+export const city = add(locationPanel, 'city');
+export const todayDate = add(locationPanel, 'today-date');
+export const todayTime = add(locationPanel, 'today-time');
+const searchPanel = add(locationPanel, 'search-panel');
+export const searchInput = add(searchPanel, 'search-input', 'input');
+searchInput.placeholder = 'New York';
+export const searchImage = add(searchPanel, 'search-icon');
+const searchIcon = new Image();
+searchIcon.src = SearchIcon;
+searchImage.appendChild(searchIcon);
+
+/* TEMPERATURE PANEL */
+export const description = add(temperaturePanel, 'description');
+export const weatherImage = add(temperaturePanel, 'weather-icon');
+export const temperature = add(temperaturePanel, 'temperature');
+const scale = add(temperaturePanel, 'scale');
+export const scaleF = add(scale, 'scaleF', 'button');
+scaleF.textContent = '°F';
+export const scaleC = add(scale, 'scaleC', 'button');
+scaleC.textContent = '°C';
+export const gif = add(temperaturePanel, 'gif');
+
+/* STATS PANEL */
+const feelsLike = add(statsPanel, 'feels-like');
+export const feelsLikeImage = add(feelsLike, 'feels-like-icon');
+const feelsLikeIcon = new Image();
+feelsLikeIcon.src = FeelsLikeIcon;
+feelsLikeImage.appendChild(feelsLikeIcon);
+const feelsLikeLabel = add(feelsLike, 'feels-like-label');
+feelsLikeLabel.textContent = 'Feels Like';
+export const feelsLikeValue = add(feelsLike, 'feels-like-value');
+
+const windSpeed = add(statsPanel, 'wind-speed');
+export const windSpeedImage = add(windSpeed, 'wind-speed-icon');
+const windSpeedIcon = new Image();
+windSpeedIcon.src = WindSpeedIcon;
+windSpeedImage.appendChild(windSpeedIcon);
+const windSpeedLabel = add(windSpeed, 'wind-speed-label');
+windSpeedLabel.textContent = 'Wind Speed';
+export const windSpeedValue = add(windSpeed, 'wind-speed-value');
+
+const rainChance = add(statsPanel, 'rain-chance');
+export const rainChanceImage = add(rainChance, 'rain-chance-icon');
+const rainChanceIcon = new Image();
+rainChanceIcon.src = RainChanceIcon;
+rainChanceImage.appendChild(rainChanceIcon);
+const rainChanceLabel = add(rainChance, 'rain-chance-label');
+rainChanceLabel.textContent = 'Chance of Rain';
+export const rainChanceValue = add(rainChance, 'rain-chance-value');
+
+const humidity = add(statsPanel, 'humidity');
+export const humidityImage = add(humidity, 'humidity-icon');
+const humidityIcon = new Image();
+humidityIcon.src = HumidityIcon;
+humidityImage.appendChild(humidityIcon);
+const humidityLabel = add(humidity, 'humidity-label');
+humidityLabel.textContent = 'Humidity';
+export const humidityValue = add(humidity, 'humidity-value');
+
+/* DAY PANEL */
+
+/* WEEK PANEL */
--- src/index.js
+++ src/index.js
@@ -1,22 +1,78 @@
 import './style.css';
-// import CloudyIcon from './icons/clouds.png';
+import CloudyIcon from './icons/clouds.png';
 // import FoggyIcon from './icons/weather-fog.png';
 // import HailyIcon from './icons/weather-hail.png';
 // import StormyIcon from './icons/weather-lighning.png';
 // import OvercastIcon from './icons/weather-partly-cloudy.png';
 // import RainyIcon from './icons/weather-pouring.png';
-// import ChanceOfRainIcon from './icons/weather-rainy.png';
 // import SnowyIcon from './icons/weather-snowy-heavy.png';
 // import SunnyIcon from './icons/weather-sunny.png';
 // import WindyIcon from './icons/weather-dust.png';
-// import FeelsLikeIcon from './icons/thermometer-lines.png';
-// import HumidityIcon from './icons/water-percent.png';
-// import WindSpeedIcon from './icons/weather-windy.png';
-// import CelsiusIcon from './icons/temperature-celsius.png';
-// import FahrenheitIcon from './icons/temperature-fahrenheit.png';
-// import SearchIcon from './icons/magnify.png';
 
 import {
-  content,
-  container,
+  city,
+  todayDate,
+  todayTime,
+  description,
+  weatherImage,
+  temperature,
+  gif,
+  feelsLikeValue,
+  windSpeedValue,
+  rainChanceValue,
+  humidityValue,
+  dayPanel,
+  weekPanel,
 } from './dom';
+
+// helper functions
+function addItem(where, cls, tag = 'div') {
+  const element = document.createElement(tag);
+  element.className = cls;
+  return where.appendChild(element);
+}
+
+// location panel
+city.textContent = 'New York';
+todayDate.textContent = 'Monday, August 15th, 2022';
+todayTime.textContent = '7:47 pm';
+
+// temperature panel
+description.textContent = 'Light Rain';
+const weatherIcon = new Image();
+weatherIcon.src = CloudyIcon;
+weatherImage.appendChild(weatherIcon);
+temperature.textContent = '24°C';
+gif.textContent = 'GIF';
+
+// stats panel
+feelsLikeValue.textContent = '22°C';
+windSpeedValue.textContent = '28.6 km/h';
+rainChanceValue.textContent = '0.6%';
+humidityValue.textContent = '84%';
+
+// day panel
+for (let i = 0; i < 24; i++) {
+  const hourContainer = addItem(dayPanel, 'hour-container');
+  const hour = addItem(hourContainer, 'hour');
+  hour.textContent = '9 pm';
+  const hourTemperature = addItem(hourContainer, 'hour-temperature');
+  hourTemperature.textContent = '20°C';
+  const hourIcon = new Image();
+  hourIcon.className = 'hour-icon';
+  hourIcon.src = CloudyIcon;
+  hourContainer.appendChild(hourIcon);
+}
+
+// week panel
+for (let i = 0; i < 8; i++) {
+  const dayContainer = addItem(weekPanel, 'week-container');
+  const day = addItem(dayContainer, 'day');
+  day.textContent = 'Thursday';
+  const dayTemperature = addItem(dayContainer, 'day-temperature');
+  dayTemperature.textContent = '19°C';
+  const dayIcon = new Image();
+  dayIcon.className = 'day-icon';
+  dayIcon.src = CloudyIcon;
+  dayContainer.appendChild(dayIcon);
+}
